e7355bc1b53de2e0a8a2719f8f92e1a5d213c2ff
ytpham <ytpham@ucsd.edu>
2015-12-06 15:17:18 -0800

    Make the script more controllable to multiple nrf dev

--- send_rf.py
+++ send_rf.py
@@ -5,31 +5,48 @@ import sys
 import time
 import spidev
 
+# Usage  python send_rf.py <device#> <int>
+try:
+	GPIO.setmode(GPIO.BCM)
+	Devices = {
+				'host'	 : [0xf0,0xf0,0xf0,0xf0,0xe1] ,
+				'device1' :[0xe0,0xe0,0xe0,0xe0,0xe1] ,
+				'device2' :[0xe0,0xe0,0xe0,0xe0,0xe2] ,
+				'device3' :[0xe0,0xe0,0xe0,0xe0,0xe3] ,
+				'device4' :[0xe0,0xe0,0xe0,0xe0,0xe4] ,
+				'device5' :[0xe0,0xe0,0xe0,0xe0,0xe5] ,
+			}
+	#pipes = [[ 0xf0, 0xf0 ,0xf0,0xf0,0xe1 ],
+	#	[ 0xf0, 0xf0 ,0xf0,0xf0,0xd2 ] ]
 
-GPIO.setmode(GPIO.BCM)
-pipes = [[ 0xf0, 0xf0 ,0xf0,0xf0,0xe1 ],
-	[ 0xf0, 0xf0 ,0xf0,0xf0,0xd2 ] ]
+	radio = NRF24(GPIO,spidev.SpiDev())
+	radio.begin(0,25)
+	radio.setPayloadSize(32)
+	radio.setChannel(0x4c)
+	radio.setDataRate(NRF24.BR_1MBPS)
+	radio.setPALevel(NRF24.PA_MAX)
+	
+	# Need compliance
+	#radio.setAutoAck(True)
+	#radio.enableDynamicPayloads()
+	radio.enableAckPayload()
+	
+	buf =  [int(sys.argv[2])]
+	dev =  str(sys.argv[1])
+	
+	radio.openWritingPipe(Devices['host'])
+	radio.openReadingPipe(1,Devices[dev])
+	radio.printDetails()
 
-radio = NRF24(GPIO,spidev.SpiDev())
-radio.begin(0,25)
-radio.setPayloadSize(32)
-radio.setChannel(0x4c)
-radio.setDataRate(NRF24.BR_1MBPS)
-radio.setPALevel(NRF24.PA_MAX)
-
-#radio.setAutoAck(True)
-#radio.enableDynamicPayloads()
-radio.enableAckPayload()
-
-radio.openWritingPipe(pipes[0])
-radio.openReadingPipe(1, pipes[1])
-radio.printDetails()
-
-buf =  list(sys.argv[1])
-buf =  [int(sys.argv[1])]
-
-radio.stopListening()
-radio.write(buf)
-print "Sent : " , buf
-radio.startListening()
-
+	radio.stopListening()
+	radio.write(buf)
+	print "Sent : " , buf
+	radio.startListening()
+except Exception as ex:
+	print ex
+finally:
+	print "Prepare for cleanup"
+	GPIO.cleanup()
+	
+	
+	
