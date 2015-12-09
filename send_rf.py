592573ee4f28cf5d27d3435619986a154d0dfe13
Khang Pham <khangpham@ymail.com>
2015-12-09 04:41:13 +0000

    Added ack from client to host , by verify RX address

--- send_rf.py
+++ send_rf.py
@@ -7,6 +7,8 @@ import spidev
 
 # Usage  python send_rf.py <device#> <int>
 try:
+	TIMEOUT = 10
+	STATUS  = "Unknown"
 	GPIO.setmode(GPIO.BCM)
 	Devices = {
 				'host'	 : [0xf0,0xf0,0xf0,0xf0,0xe1] ,
@@ -15,19 +17,20 @@ try:
 				'device3' :[0xe0,0xe0,0xe0,0xe0,0xe3] ,
 				'device4' :[0xe0,0xe0,0xe0,0xe0,0xe4] ,
 				'device5' :[0xe0,0xe0,0xe0,0xe0,0xe5] ,
+				'device6' :[0xe0,0xe0,0xe0,0xe0,0xe6] ,
+				'device7' :[0xe0,0xe0,0xe0,0xe0,0xe7] ,
+				'device8' :[0xe0,0xe0,0xe0,0xe0,0xe8] ,
+				'device9' :[0xe0,0xe0,0xe0,0xe0,0xe9] ,
 			}
 
 	radio = NRF24(GPIO,spidev.SpiDev())
 	radio.begin(0,25)
+	STATUS = "Begin"
 	radio.setPayloadSize(32)
 	radio.setChannel(0x4c)
 	radio.setDataRate(NRF24.BR_1MBPS)
 	radio.setPALevel(NRF24.PA_MAX)
 	
-	# Need compliance
-	#radio.setAutoAck(True)
-	#radio.enableDynamicPayloads()
-	radio.enableAckPayload()
 	
 	buf =  [int(sys.argv[2])]
 	dev =  str(sys.argv[1])
@@ -38,10 +41,24 @@ try:
 
 	radio.stopListening()
 	radio.write(buf)
+	STATUS = "Sent"
 	print "Sent : " , buf
 	radio.startListening()
+	count = 1;
+	recv_buffer = []
+	while count <TIMEOUT:
+		if radio.available():
+			radio.read(recv_buffer)
+			if recv_buffer[:5][::-1] == Devices[dev]:
+				status = "Success"
+			
+			break
+		status = "Failed"
+		time.sleep(1)
+	print status
 except Exception as ex:
 	print ex
+	print STATUS
 	print "Usage: sudo python send_rf.py <device#> <int>"
 finally:
 	# Clean up GPIO if used
